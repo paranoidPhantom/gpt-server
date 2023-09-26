6d28c2ad4a25f1335028ba8f2f63db712dded936
Andrei <andrei.hudalla@outlook.com>
2023-09-26 18:12:55 -0500

    Various fixes + changed functionality

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import OpenAI from "openai";
-import { config } from "dotenv"
-config()
+import { config } from "dotenv";
+import express, { response } from "express";
+config();
+
+const app = express();
+const port = 3000;
 
 const openai = new OpenAI();
 
@@ -10,25 +14,30 @@ const requestGPT = async (content) => {
         model: "gpt-3.5-turbo",
     });
 
-    return completion.choices[0].message.content
-}
+    const responseContent = completion.choices[0].message.content;
+    return responseContent
+};
 
 const understandResponse = async (input, options) => {
     const request = `You are given a statement: '${input}' 
-    try to understand which option best descrives this statement: '${options.join("', '")}'.
-    The important part is that you are required to respond with only the index of the option (from 1 to ${options.length}).
-    No text only one number. If none of the options sufficiently match any of the statements return just '0'.`
-    console.log(request)
-    const response = await requestGPT(request)
-    const optionIndex = parseInt(response)
-    return optionIndex !== 0 ? options[optionIndex - 1] : "No option matches the input."
-}
+    try to understand which option best descrives this statement: '${options.join(
+        "', '"
+    )}'.
+    The important part is that you are required to respond with only the index of the option (from 1 to ${
+        options.length
+    }).
+    No text only one number. If none of the options sufficiently match any of the statements, return a logical response to the statement and prefix it with a '!'.`;
+    const response = await requestGPT(request);
+    return response
+};
 
-const conclusion = await understandResponse(``, [
-    "The murderer is Bob",
-    "The person is tired",
-    "The murderer is Alice",
-    "The person is hungry",
-    "Life is hard"
-])
-console.log(conclusion)
+app.get("/api", async (req, res) => {
+    const { input, options: optionsString } = req.query;
+    const options = optionsString.split(",");
+    const conclusion = await understandResponse(input, options);
+    res.send(conclusion)
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
